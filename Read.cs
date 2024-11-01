bb2cfa9a79cdff83585fc7b47d1c161ad1c29b4a
volkrum123 <stiaanmegit2@gmail.com>
2024-11-01 15:11:57 +0200

    I have created a Read class that will read all the text off of a text file and store that text in a string list.

---
+++ Read.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    internal class Read
+    {
+        public string filename = @"C:\Users\Cash\OneDrive - belgiumcampus.ac.za\Documents\Belgium\Year2\PRG282\Project\282Project\StudentManagementSystem\Content.txt";
+
+        public List<string> read()
+        {
+            List<string> list = new List<string>();
+            list = File.ReadAllLines(filename).ToList();
+            return list;
+        }
+    }
+}
