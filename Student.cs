2bac2ba050957eb6a374f38af0269ed7f59bad03
volkrum123 <stiaanmegit2@gmail.com>
2024-11-01 14:42:22 +0200

    I created a student class to get and set the textbox values.

---
+++ Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    internal class Student
+    {
+        string studentID, name;
+        int age;
+        string course;
+
+        public Student()
+        {
+
+        }
+        public Student(string studentID, string name, int age, string course)
+        {
+            this.StudentID = studentID;
+            this.Name = name;
+            this.Age = age;
+            this.Course = course;
+        }
+
+        public string StudentID { get => studentID; set => studentID = value; }
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public string Course { get => course; set => course = value; }
+    }
+}
