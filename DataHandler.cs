bfeb2034f95c8c817f2645a21a97bd371a99589c
volkrum123 <stiaanmegit2@gmail.com>
2024-11-01 15:19:32 +0200

    I created a DataHandler class to format the data in the string list that it recieves from the read class. It formats the data and stores it in a student object list.

---
+++ DataHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    internal class DataHandler
+    {
+        List<Student> students = new List<Student>();
+
+        public List<Student> format(List<string> list)
+        {
+            foreach (string item in list)
+            {
+                string[] items = item.Split(',');
+                Student person = new Student(items[0], items[1], Convert.ToInt32(items[2]), items[3]);
+                students.Add(person);
+            }
+            return students;
+        }
+    }
+}
