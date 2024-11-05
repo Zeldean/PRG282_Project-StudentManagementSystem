0a067e08de2459a7c5c1711fda5b755c4b5abe51
JPgitstuff <jpseaman0001@gmail.com>
2024-11-05 11:10:43 +0200

    Add files via upload
    Added a Write class.

---
+++ Write.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace StudentManagementSystem
+{
+    internal class Write
+    {
+        public string filePath = Path.Combine(".", "Content.txt");
+        public void write(List<Student> studentlist)
+        {
+            var lines = studentlist.Select(s => $"{s.StudentID},{s.Name},{s.Age},{s.Course}").ToArray();
+             File.WriteAllLines(filePath, lines);
+        }
+        
+        
+    }
+}
